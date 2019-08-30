ebe111de4e6bbb7212b287c1046971600651b2fc
OMNES Thomas <thomas_omnes@groupe-sma.fr>
2019-08-30 09:16:08 +0200

    first commit

---
+++ roApp/src/main/java/sma/fr/bt/java/app/roApp/RoAppApplication.java
@@ -0,0 +1,13 @@
+package sma.fr.bt.java.app.roApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoAppApplication.class, args);
+	}
+
+}
---
+++ roApp/src/test/java/sma/fr/bt/java/app/roApp/RoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package sma.fr.bt.java.app.roApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
